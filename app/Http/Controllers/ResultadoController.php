b5ef74b0743b6d61bacfe14f8942b7a32a790284
Marcos Fábio <marcaum54@gmail.com>
2021-01-06 20:07:52 -0300

    Finalizando o GET /resultado/por-idade

--- app/Http/Controllers/ResultadoController.php
+++ app/Http/Controllers/ResultadoController.php
@@ -26,9 +26,8 @@ class ResultadoController extends Controller
                 });
             })->orderBy(DB::raw('hora_fim - hora_ini'))->get()->toArray();
 
-            foreach ($json[$tipo] as $key => &$row) {
+            foreach ($json[$tipo] as $key => &$row)
                 $row['posicao'] = $key + 1;
-            }
         }
 
         return response()->json($json);
@@ -36,5 +35,45 @@ class ResultadoController extends Controller
 
     public function porIdade()
     {
+        $idades = [];
+
+        $ranges = [
+            '18 – 25 anos' => [18, 25],
+            '25 – 35 anos' => [25, 35],
+            '35 – 45 anos' => [35, 45],
+            '45 – 55 anos' => [45, 55],
+            'Acima de 55 anos' => [55],
+        ];
+
+        foreach ($ranges as $key => $range) {
+            if (count($range) == 2) {
+                foreach (range($range[0], $range[1]) as $idade) {
+                    $idades[$idade] = $key;
+                }
+            } else {
+                $idades[$range[0]] = $key;
+            }
+        }
+
+        $json = [];
+
+        foreach (array_keys($ranges) as $range_idade) {
+            $json[$range_idade] = [];
+        }
+
+        foreach (Prova::TIPOS as $tipo) {
+            $rows = Resultado::whereHas('corredorProva', function ($query) use ($tipo) {
+                return $query->whereHas('prova', function ($query) use ($tipo) {
+                    return $query->where('tipo', $tipo);
+                });
+            })->orderBy(DB::raw('hora_fim - hora_ini'))->get()->toArray();
+
+            foreach ($rows as &$row) {
+                $idade = isset($idades[$row['idade']]) ? $idades[$row['idade']] : end($idades);
+                $json[$idade][$tipo][] = $row;
+            }
+        }
+
+        return response()->json($json);
     }
 }
